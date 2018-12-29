4668913ea956c2f2b6fdb9f8f1ff931354f7605c
smanne <sandeep@hellothinkster.com>
2018-12-29 13:19:39 +0530

    Inital commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || '3000';
+
+app.get('/status', (req, res, next) => {
+    res.status(200).json({
+        status: "ok"
+    })
+});
+
+app.get("/time", (req, res, next) => {
+    res.status(200).json({
+        time: new Date().toUTCString()
+    })
+})
+
+app.listen(port, function() {
+    console.log(`cors-test app listening on port ${port}`);
+})
